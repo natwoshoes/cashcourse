d9df6a3d8e025d2779ea7ad31cc294c3509fb309
natasha <mnatasha737@gmail.com>
2024-11-10 08:54:11 -0500

    just changing colors

--- pages/credit.py
+++ pages/credit.py
@@ -31,7 +31,6 @@ def create_flashcards():
             padding: 2rem;
             border-radius: 8px;
             box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
-            background-color: blue;
             margin: rem 0;
             text-align: center;
             min-height: 200px;
